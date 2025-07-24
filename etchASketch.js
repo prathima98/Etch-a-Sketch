8ff56adc9d3a8b3a4001eead7dd35dde3b4abbdb
prathima98 <40156142+prathima98@users.noreply.github.com>
2025-07-23 22:18:40 -0400

    Add extra credit details tp the project

--- etchASketch.js
+++ etchASketch.js
@@ -2,14 +2,23 @@ let container = document.querySelector(".grid-container");
 
 function buildGrid(size) {
     container.innerHTML = "";
+    let count = 0.0;
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let newDiv = document.createElement("div");
             newDiv.style.width = "16px";
             newDiv.style.height = "16px";
+            newDiv.style.opacity = "0.1";
             newDiv.style.boxSizing = "border-box";
             newDiv.addEventListener("mouseover", () => {
-                newDiv.style.backgroundColor = "black";
+                if (count < 1) {
+                    count = count + 0.1;
+                    newDiv.style.opacity = `${count}`;
+                    newDiv.style.backgroundColor = "black";
+                } else {
+                    newDiv.style.opacity = "1";
+                    newDiv.style.backgroundColor = "black";
+                }
             })
             container.append(newDiv);
         }
